50c4bc451e6b5caed3c91174e011d405a41a484a
ferjavi40 <ferjaviramirez@gmail.com>
2021-05-14 15:18:49 +0200

    feature: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OnilModule } from './onil/pages/onil.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OnilModule
   ],
   providers: [],
   bootstrap: [AppComponent]
