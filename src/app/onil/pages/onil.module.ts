20f214a6be5544bb7d6ca80009d8371e4b9986e7
ferjavi40 <ferjaviramirez@gmail.com>
2021-05-14 16:14:37 +0200

    feature: test done

--- src/app/onil/pages/onil.module.ts
+++ src/app/onil/pages/onil.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    NavbarComponent
   ]
 })
 export class OnilModule { }
